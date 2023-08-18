f22bdcd5db8b8ddae5ccbf0aa4ff05656289b2f4
Helium314 <helium314@mailbox.org>
2023-08-18 06:16:44 +0200

    make main preference show the right things

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/LanguageFilterListPreference.kt
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/LanguageFilterListPreference.kt
@@ -1,47 +1,52 @@
 package org.dslul.openboard.inputmethod.latin.settings
 
 import android.content.Context
-import android.inputmethodservice.InputMethodService
+import android.graphics.Rect
 import android.os.Build
 import android.preference.Preference
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodSubtype
 import android.widget.*
+import androidx.core.view.doOnLayout
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
-import org.dslul.openboard.inputmethod.compat.InputMethodManagerCompatWrapper
 import org.dslul.openboard.inputmethod.latin.R
 import org.dslul.openboard.inputmethod.latin.RichInputMethodManager
-import org.dslul.openboard.inputmethodcommon.InputMethodSettingsFragment
 
 class LanguageFilterListPreference(context: Context, attrs: AttributeSet) : Preference(context, attrs) {
 
     private var preferenceView: View? = null
     private val adapter = LanguageAdapter()
     private val sortedSubtypes = mutableListOf<SubtypeInfo>()
-    val searchAdapter = SearchAdapter(context,
-        { search -> sortedSubtypes.filter { it.displayName.startsWith(search) } },
-        { it.displayName }
-    )
 
     override fun onBindView(view: View?) {
         super.onBindView(view)
         preferenceView = view
-        val r = preferenceView?.findViewById<RecyclerView>(R.id.language_list)!!
-        r.adapter = adapter
+        preferenceView?.findViewById<RecyclerView>(R.id.language_list)?.adapter = adapter
         val searchField = preferenceView?.findViewById<EditText>(R.id.search_field)!!
         searchField.doAfterTextChanged { text ->
-            adapter.list = sortedSubtypes.filter { it.displayName.startsWith(text.toString()) }.toMutableList()
+            adapter.list = sortedSubtypes.filter { it.displayName.startsWith(text.toString(), ignoreCase = true) }
+        }
+        view?.doOnLayout {
+            // set correct height for recycler view, so there is no scrolling of the outside view happening
+            // not sure how, but probably this can be achieved in xml...
+            val windowFrame = Rect()
+            it.getWindowVisibleDisplayFrame(windowFrame) // rect the app has, we want the bottom (above screen bottom/navbar/keyboard)
+            val globalRect = Rect()
+            it.getGlobalVisibleRect(globalRect) // rect the view takes, we want the top (below the system language preference)
+            val recycler = it.findViewById<RecyclerView>(R.id.language_list)
+
+            val newHeight = windowFrame.bottom - globalRect.top - it.findViewById<View>(R.id.search_container).height
+            recycler.layoutParams = recycler.layoutParams.apply { height = newHeight }
         }
     }
 
-    fun setLanguages(list: List<SubtypeInfo>) {
+    fun setLanguages(list: List<SubtypeInfo>, disableSwitches: Boolean) {
         sortedSubtypes.clear()
         sortedSubtypes.addAll(list)
+        adapter.disableSwitches = disableSwitches
         adapter.list = sortedSubtypes
     }
 
@@ -50,8 +55,9 @@ class LanguageFilterListPreference(context: Context, attrs: AttributeSet) : Pref
 // todo: decide class
 class LanguageAdapter(list: List<SubtypeInfo> = listOf()) :
     RecyclerView.Adapter<LanguageAdapter.ViewHolder>() {
+    var disableSwitches = false
 
-    var list: MutableList<SubtypeInfo> = list.toMutableList()
+    var list: List<SubtypeInfo> = list
         set(value) {
             field = value
             notifyDataSetChanged() // todo: check performance, maybe better do a diff if bad?
@@ -71,78 +77,47 @@ class LanguageAdapter(list: List<SubtypeInfo> = listOf()) :
     inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         fun onBind(info: SubtypeInfo) {
             view.findViewById<TextView>(R.id.language_name).text = info.displayName
-            view.findViewById<Switch>(R.id.language_switch).isChecked = info.isEnabled
-            view.findViewById<Switch>(R.id.language_switch).setOnCheckedChangeListener { _, b ->
-                // todo: what now?
-                // todo: maybe need to copy some code from CustomInputStyleSettingsFragment
-                // i really should have checked this... apparently the only way to enable an inputmethod is using the system
-                //  or not exposing different subtypes to the system... which is better?
-                if (b) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        // calls the default menu... maybe try this, at least it might not be crashing
-//                        view.context.getSystemService(InputMethodManager::class.java).showInputMethodAndSubtypeEnabler(null);
-                    }
-                    RichInputMethodManager.getInstance().setAdditionalInputMethodSubtypes(arrayOf(info.subtype)) // nah
-                    // there is putSelectedSubtype / putSelectedInputMethod in InputMethodUtils.InputMethodSettings (frameworks/base)
-                    // and possibly i could write secure settings, but that's not really great
-                    // but this is not public enough...
+//            view.findViewById<TextView>(R.id.language_details).text = // some short info, no more than 2 lines
+            view.findViewById<TextView>(R.id.language_text).setOnClickListener {
+                // todo: click item dialog (better full screen with custom layout, not some alertDialog)
+                //  secondary locale options similar to now
+                //  add/remove dictionary thing, like now
+                //   but also for secondary locales
+                //  option to change / adjust layout (need to check how exactly)
+            }
+            view.findViewById<Switch>(R.id.language_switch).apply {
+                // take care: isChecked changes if the language is scrolled out of view and comes back!
+                // disable the change listener when setting the checked status on scroll
+                // so it's only triggered on user interactions
+                setOnCheckedChangeListener(null)
+                isChecked = info.isEnabled
+                isEnabled = !disableSwitches
+                setOnCheckedChangeListener { _, b ->
+                    // todo: what now?
+                    // todo: maybe need to copy some code from CustomInputStyleSettingsFragment
+                    // i really should have checked this... apparently the only way to enable an inputmethod is using the system
+                    //  or not exposing different subtypes to the system... which is better?
+                    if (b) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            // calls the default menu... maybe try this, at least it might not be crashing
+//                          view.context.getSystemService(InputMethodManager::class.java).showInputMethodAndSubtypeEnabler(null);
+                        }
+                        RichInputMethodManager.getInstance()
+                            .setAdditionalInputMethodSubtypes(arrayOf(info.subtype)) // nah
+                        // there is putSelectedSubtype / putSelectedInputMethod in InputMethodUtils.InputMethodSettings (frameworks/base)
+                        // and possibly i could write secure settings, but that's not really great
+                        // but this is not public enough...
 /*                    val a: InputMethodService
-                    a.InputMethodImpl().
-                    a.InputMethodSessionImpl().
-                    InputMethodManager
-                    InputMethodManagerCompatWrapper*/
-                } else {
+                        a.InputMethodImpl().
+                        a.InputMethodSessionImpl().
+                        InputMethodManager
+                        InputMethodManagerCompatWrapper*/
+                    } else {
 //                    RichInputMethodManager.getInstance().
+                    }
                 }
             }
             // todo: set other text
-        }
-    }
-}
-
-// todo: remove?
-// adapter with filtering, taken from old version of StreetComplete
-class SearchAdapter<T>(
-    private val context: Context,
-    private val filterQuery: (term: String) -> List<T>,
-    private val convertToString: (T) -> String
-) : BaseAdapter(), Filterable {
-
-    private val filter = SearchFilter()
-
-    private var items: List<T> = emptyList()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    override fun getCount(): Int = items.size
-    override fun getItem(position: Int): T = items[position]
-    override fun getItemId(position: Int): Long = position.toLong()
-    override fun getFilter() = filter
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = convertView ?: LayoutInflater.from(context).inflate(android.R.layout.simple_dropdown_item_1line, parent, false)
-        (view as TextView).text = filter.convertResultToString(getItem(position))
-        return view
-    }
-
-    inner class SearchFilter : Filter() {
-        override fun performFiltering(constraint: CharSequence?) = FilterResults().also {
-            val term = constraint?.toString() ?: ""
-            val results = filterQuery(term)
-            it.count = results.size
-            it.values = results
-        }
-
-        override fun publishResults(constraint: CharSequence?, results: FilterResults) {
-            // results should always come from performFiltering, but still got a crash report with
-            // NPE here, which happens on click ok (and not actually anything where filtering happens)
-            (results?.values as? List<T>)?.let { items = it }
-        }
-
-        override fun convertResultToString(resultValue: Any?): CharSequence {
-            return (resultValue as? T?)?.let(convertToString) ?: super.convertResultToString(resultValue)
         }
     }
 }
--- app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/LanguageSettingsFragment.kt
+++ app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/LanguageSettingsFragment.kt
@@ -1,6 +1,7 @@
 package org.dslul.openboard.inputmethod.latin.settings
 
 import android.os.Bundle
+import android.preference.TwoStatePreference
 import android.view.inputmethod.InputMethodSubtype
 import androidx.core.app.LocaleManagerCompat
 import org.dslul.openboard.inputmethod.latin.R
@@ -11,6 +12,7 @@ import org.dslul.openboard.inputmethod.latin.utils.SubtypeLocaleUtils
 import java.util.Locale
 
 
+@Suppress("Deprecation") // yes everything here is deprecated, but only work on this if really necessary
 class LanguageSettingsFragment : SubScreenFragment() {
 
     private val sortedSubtypes = mutableListOf<SubtypeInfo>()
@@ -18,21 +20,12 @@ class LanguageSettingsFragment : SubScreenFragment() {
     private val systemLocales = mutableListOf<Locale>()
 
     // todo:
-    //  * xml file
-    //     that "system languages" switch on top
-    //      if disabling, set those languages enabled like it would have been done manually
-    //      where is the text coming from? can't find it in this repo, so probably system
-    //     then the filter editText (with magnifying glass icon)
-    //     then recyclerview with the (filtered) subtype list
-    //      if "system languages" is enabled, only show those and don't allow disabling
-    //      on disabling, update the list immediately? or don't yet re-order in case it was accidental?
-    //  * click item dialog
-    //     add/remove dictionary thing, like now
-    //     secondary locale options like now, but also offer adding a dictionary (or even multiple? after all it's using stuff from another language...)
-    //      idea: add some user-defined factor for each language? suggestion would be multiplied by this (probably should be between 0.8 and 1.2 to work reasonable)
-    //     option to change / adjust layout (need to check how exactly)
     //  * where / how to actually store changed language selection?
     //     probably simple keyboard can help here
+    //     check out https://github.com/rkkr/simple-keyboard/pull/291/files
+    //     most of the stuff is in RichInputMethodManager (not surprising)
+    //  * need some default to fall back if nothing is enabled
+    //     -> system locales, and if not existing en_US
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,20 +34,37 @@ class LanguageSettingsFragment : SubScreenFragment() {
 
         enabledSubtypes.addAll(RichInputMethodManager.getInstance().getMyEnabledInputMethodSubtypeList(true))
         systemLocales.addAll(getSystemLocales())
-        loadSubtypes()
-        val thatPref = findPreference("language_filter") as LanguageFilterListPreference
-        thatPref.setLanguages(sortedSubtypes)
+        (findPreference("pref_system_languages") as TwoStatePreference).setOnPreferenceChangeListener { _, b ->
+            loadSubtypes(b as Boolean)
+            true
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadSubtypes((findPreference("pref_system_languages") as TwoStatePreference).isChecked)
     }
 
     // performance not great (0.1-0.2 s on S4 mini), but still acceptable
-    // todo: different behavior if only showing system locales!
-    private fun loadSubtypes() {
+    // todo: less duplicate code?
+    private fun loadSubtypes(systemOnly: Boolean) {
+        sortedSubtypes.clear()
         val inputMethodInfo = RichInputMethodManager.getInstance().inputMethodInfoOfThisIme
         val l = mutableListOf<InputMethodSubtype>()
         for (i in 0 until inputMethodInfo.subtypeCount) {
             l.add(inputMethodInfo.getSubtypeAt(i))
             // subtype.locale is locale (deprecated, but working)
             // subtype.languageTag is often not set (in method.xml), and additionally requires api24
+        }
+
+        if (systemOnly) {
+            systemLocales.mapNotNull { locale ->
+                val localeString = locale.toString()
+                val subtype = l.firstOrNull { it.locale == localeString }
+                subtype?.toSubtypeInfo()
+            }.sortedBy { it.displayName }.let { sortedSubtypes.addAll(it) }
+            (findPreference("pref_language_filter") as LanguageFilterListPreference).setLanguages(sortedSubtypes, systemOnly)
+            return
         }
 
         // add enabled subtypes
@@ -78,7 +88,7 @@ class LanguageSettingsFragment : SubScreenFragment() {
             subtype?.toSubtypeInfo()
         }?.sortedBy { it.displayName }?.let { sortedSubtypes.addAll(it) }
 
-        // add subtypes for device locales, todo: don't duplicate code
+        // add subtypes for device locales
         systemLocales.filterNot { it.toString() in currentLocales }.mapNotNull { locale ->
             val localeString = locale.toString()
             val subtype = l.firstOrNull { it.locale == localeString } // only takes first, but multiple types should be rare here anyway
@@ -89,6 +99,9 @@ class LanguageSettingsFragment : SubScreenFragment() {
 
         // add the remaining ones
         sortedSubtypes.addAll(l.map { it.toSubtypeInfo() }.sortedBy { it.displayName })
+
+        // set languages
+        (findPreference("pref_language_filter") as LanguageFilterListPreference).setLanguages(sortedSubtypes, systemOnly)
     }
 
     private fun InputMethodSubtype.toSubtypeInfo(isEnabled: Boolean = false) =
