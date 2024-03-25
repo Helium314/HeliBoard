c4fcb74b0ed5054caf8090cfb5a86fbc80641098
Helium314 <helium314@mailbox.org>
2024-03-25 20:21:50 +0100

    disable IS_HARDWARE_KEYBOARD_SUPPORTED not working properly when enabled (e.g. ctrl+v)

--- app/src/main/java/helium314/keyboard/latin/define/ProductionFlags.kt
+++ app/src/main/java/helium314/keyboard/latin/define/ProductionFlags.kt
@@ -7,9 +7,11 @@
 package helium314.keyboard.latin.define
 
 object ProductionFlags {
-    const val IS_HARDWARE_KEYBOARD_SUPPORTED = true // was set to true in hangul branch
-    // todo: test whether there are issues
-    //  hangul dev apparently did use it at least the hangul hardware event decoder in latinIme suggests it
+    const val IS_HARDWARE_KEYBOARD_SUPPORTED = false
+    // todo: make it work
+    //  was set to true in hangul branch (and there is the hangul hardware event decoder in latinIme)
+    //  but disabled again because this breaks ctrl+c / ctrl+v, and most likely other things
+    //  so it looks like the HardwareKeyboardEventDecoder needs some work before it's ready
 
     /**
      * Include all suggestions from all dictionaries in
