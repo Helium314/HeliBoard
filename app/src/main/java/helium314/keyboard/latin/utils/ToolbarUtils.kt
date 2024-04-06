38126d2840b8335c98a95193ed4cb6e3dc3ea6ce
codokie <151087174+codokie@users.noreply.github.com>
2024-04-06 19:11:28 +0300

    add close history view toolbar button (#649)

--- app/src/main/java/helium314/keyboard/latin/utils/ToolbarUtils.kt
+++ app/src/main/java/helium314/keyboard/latin/utils/ToolbarUtils.kt
@@ -56,6 +56,7 @@ fun getCodeForToolbarKey(key: ToolbarKey) = when (key) {
     FULL_RIGHT -> KeyCode.MOVE_END_OF_LINE
     SELECT_WORD -> KeyCode.CLIPBOARD_SELECT_WORD
     CLEAR_CLIPBOARD -> null // not managed via code input
+    CLOSE_HISTORY -> KeyCode.ALPHA
 }
 
 private fun getStyleableIconId(key: ToolbarKey) = when (key) {
@@ -77,6 +78,7 @@ private fun getStyleableIconId(key: ToolbarKey) = when (key) {
     FULL_LEFT -> R.styleable.Keyboard_iconFullLeft
     FULL_RIGHT -> R.styleable.Keyboard_iconFullRight
     SELECT_WORD -> R.styleable.Keyboard_iconSelectWord
+    CLOSE_HISTORY -> R.styleable.Keyboard_iconCloseView
 }
 
 fun getToolbarIconByName(name: String, context: Context): Drawable? {
@@ -91,12 +93,12 @@ fun getToolbarIconByName(name: String, context: Context): Drawable? {
 // names need to be aligned with resources strings (using lowercase of key.name)
 enum class ToolbarKey {
     VOICE, CLIPBOARD, UNDO, REDO, SETTINGS, SELECT_ALL, SELECT_WORD, COPY, ONE_HANDED, LEFT, RIGHT, UP, DOWN,
-    FULL_LEFT, FULL_RIGHT, INCOGNITO, AUTOCORRECT, CLEAR_CLIPBOARD
+    FULL_LEFT, FULL_RIGHT, INCOGNITO, AUTOCORRECT, CLEAR_CLIPBOARD, CLOSE_HISTORY
 }
 
 fun toToolbarKeyString(keys: Collection<ToolbarKey>) = keys.joinToString(";") { it.name }
 
-val defaultToolbarPref = entries.filterNot { it == CLEAR_CLIPBOARD }.joinToString(";") {
+val defaultToolbarPref = entries.filterNot { it == CLEAR_CLIPBOARD || it == CLOSE_HISTORY }.joinToString(";") {
     when (it) {
         INCOGNITO, AUTOCORRECT, UP, DOWN, ONE_HANDED, FULL_LEFT, FULL_RIGHT -> "${it.name},false"
         else -> "${it.name},true"
