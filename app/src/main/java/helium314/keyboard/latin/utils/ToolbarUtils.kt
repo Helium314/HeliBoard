f5ed4b3a74b887b26bc19a648604f04840a0ca48
devycarol <dcbllrd@gmail.com>
2024-07-08 11:17:56 -0600

    long-press 'copy' to cut text

--- app/src/main/java/helium314/keyboard/latin/utils/ToolbarUtils.kt
+++ app/src/main/java/helium314/keyboard/latin/utils/ToolbarUtils.kt
@@ -71,7 +71,7 @@ fun getCodeForToolbarKeyLongClick(key: ToolbarKey) = when (key) {
     REDO -> KeyCode.UNDO
     SELECT_ALL -> KeyCode.CLIPBOARD_SELECT_WORD
     SELECT_WORD -> KeyCode.CLIPBOARD_SELECT_ALL
-    COPY -> KeyCode.CLIPBOARD_COPY_ALL
+    COPY -> KeyCode.CLIPBOARD_CUT
     PASTE -> KeyCode.CLIPBOARD
     LEFT -> KeyCode.WORD_LEFT
     RIGHT -> KeyCode.WORD_RIGHT
