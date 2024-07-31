50e51ce7c969eb6b40e350a997fd025576d9f314
kalyani <Kalyani22@navgurukul.org>
2024-07-31 21:14:37 +0530

    button ui setup done

--- app/src/main/java/helium314/keyboard/latin/setup/MainActivity.kt
+++ app/src/main/java/helium314/keyboard/latin/setup/MainActivity.kt
@@ -1,26 +1,34 @@
 package helium314.keyboard.latin.setup
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.roshaan.multiplebottomsheets.MultipleSheetsContainer
-import com.roshaan.multiplebottomsheets.REMOVE_LOCK
-import helium314.keyboard.bottomsheets.SheetOneFragment
-import helium314.keyboard.bottomsheets.SheetThreeFragment
-import helium314.keyboard.bottomsheets.SheetTwoFragment
 import helium314.keyboard.latin.R
+import helium314.keyboard.latin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val sheetContainer = findViewById<MultipleSheetsContainer>(R.id.sheetContainer)
-        sheetContainer.addFragment(0, SheetOneFragment())
-        sheetContainer.addFragment(1, SheetTwoFragment())
-        sheetContainer.addFragment(2, SheetThreeFragment())
-
-        sheetContainer.lockedSheetIndex = REMOVE_LOCK
+        // Set up click listeners
+        binding.enableId.setOnClickListener {
+            binding.textViewInstruction.visibility = View.VISIBLE
+            binding.setupOscar.visibility = View.GONE
+            binding.tryOscar.visibility = View.GONE
+        }
+        binding.setupID.setOnClickListener {
+            binding.textViewInstruction.visibility = View.GONE
+            binding.setupOscar.visibility = View.VISIBLE
+            binding.tryOscar.visibility = View.GONE
+        }
+        binding.openKeyboardId.setOnClickListener {
+            binding.textViewInstruction.visibility = View.GONE
+            binding.setupOscar.visibility = View.GONE
+            binding.tryOscar.visibility = View.VISIBLE
+        }
     }
-
 }
