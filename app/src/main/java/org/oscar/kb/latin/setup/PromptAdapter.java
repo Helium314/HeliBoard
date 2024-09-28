b9e04a982bb230c5e921a27e63f1f768a164f28e
suraj <surajkumarsahani1997@gmail.com>
2024-09-28 22:39:07 +0530

    copy to clipboard

--- app/src/main/java/org/oscar/kb/latin/setup/PromptAdapter.java
+++ app/src/main/java/org/oscar/kb/latin/setup/PromptAdapter.java
@@ -1,9 +1,14 @@
 package org.oscar.kb.latin.setup;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import org.oscar.kb.R;
@@ -62,6 +67,13 @@ public class PromptAdapter extends RecyclerView.Adapter<PromptAdapter.PromptView
             originalTranscription = itemView.findViewById(R.id.tvAIOutput);
             timestampTextView = itemView.findViewById(R.id.timestampTextView);
             //AITranscription = itemView.findViewById(R.id.tvAIOutput);
+
+            // Long press listener for original transcription
+            originalTranscription.setOnLongClickListener(v -> {
+                copyToClipboard(itemView.getContext(), originalTranscription.getText().toString());
+                return true; // Indicate that the long press was handled
+            });
+
         }
 
         public void bind(Prompt prompt) {
@@ -80,6 +92,13 @@ public class PromptAdapter extends RecyclerView.Adapter<PromptAdapter.PromptView
                 //AITranscription.setText(prompt.getAiOutput());
 
         }
+    }
+
+    private static void copyToClipboard(Context context, String text) {
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied Text", text);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_SHORT).show();
     }
 }
 
