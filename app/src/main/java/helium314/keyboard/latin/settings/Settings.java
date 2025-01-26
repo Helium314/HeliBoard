f45f69f8a2639d91992bce6aa7c16a2d4b59f4c8
Helium314 <helium314@mailbox.org>
2025-01-26 09:10:07 +0100

    add setting for adjusting bottom padding in landscape mode
    fixes some issue I can't currently find
    slight issue: will remove the bottom padding on large tables (sw768-land), users need to get it back by switching to 100%

--- app/src/main/java/helium314/keyboard/latin/settings/Settings.java
+++ app/src/main/java/helium314/keyboard/latin/settings/Settings.java
@@ -112,6 +112,7 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
     public static final String PREF_SPLIT_SPACER_SCALE = "split_spacer_scale";
     public static final String PREF_KEYBOARD_HEIGHT_SCALE = "keyboard_height_scale";
     public static final String PREF_BOTTOM_PADDING_SCALE = "bottom_padding_scale";
+    public static final String PREF_BOTTOM_PADDING_SCALE_LANDSCAPE = "bottom_padding_scale_landscape";
     public static final String PREF_SPACE_HORIZONTAL_SWIPE = "horizontal_space_swipe";
     public static final String PREF_SPACE_VERTICAL_SWIPE = "vertical_space_swipe";
     public static final String PREF_DELETE_SWIPE = "delete_swipe";
@@ -497,6 +498,12 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
     public void writeOneHandedModeGravity(final int gravity) {
         mPrefs.edit().putInt(PREF_ONE_HANDED_GRAVITY_PREFIX +
                 (getCurrent().mDisplayOrientation == Configuration.ORIENTATION_PORTRAIT), gravity).apply();
+    }
+
+    public static float readBottomPaddingScale(final SharedPreferences prefs, final boolean landscape) {
+        if (landscape)
+            return prefs.getFloat(PREF_BOTTOM_PADDING_SCALE_LANDSCAPE, 0f);
+        return prefs.getFloat(PREF_BOTTOM_PADDING_SCALE, SettingsValues.DEFAULT_SIZE_SCALE);
     }
 
     public static boolean readHasHardwareKeyboard(final Configuration conf) {
--- app/src/main/java/helium314/keyboard/latin/settings/SettingsValues.java
+++ app/src/main/java/helium314/keyboard/latin/settings/SettingsValues.java
@@ -262,7 +262,7 @@ public class SettingsValues {
                 prefs.getBoolean(Settings.PREF_GESTURE_SPACE_AWARE, false)
         );
         mSpacingAndPunctuations = new SpacingAndPunctuations(res, mUrlDetectionEnabled);
-        mBottomPaddingScale = prefs.getFloat(Settings.PREF_BOTTOM_PADDING_SCALE, DEFAULT_SIZE_SCALE);
+        mBottomPaddingScale = Settings.readBottomPaddingScale(prefs, mDisplayOrientation == Configuration.ORIENTATION_LANDSCAPE);
         mLongPressSymbolsForNumpad = prefs.getBoolean(Settings.PREFS_LONG_PRESS_SYMBOLS_FOR_NUMPAD, false);
         mAutoShowToolbar = prefs.getBoolean(Settings.PREF_AUTO_SHOW_TOOLBAR, false);
         mAutoHideToolbar = readSuggestionsEnabled(prefs) && prefs.getBoolean(Settings.PREF_AUTO_HIDE_TOOLBAR, false);
