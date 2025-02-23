53a5cb0c929670f73a0b8bef31172dfd792a0d89
Helium314 <helium314@mailbox.org>
2025-02-23 18:38:07 +0100

    upgrade compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,11 +109,11 @@ dependencies {
 
     // compose
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.7")
     implementation("sh.calvin.reorderable:reorderable:2.4.2") // for easier re-ordering
     implementation("com.github.skydoves:colorpicker-compose:1.1.2") // for user-defined colors
 
