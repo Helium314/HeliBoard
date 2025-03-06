073318d7d00bab5c07967f4d0f8c4d5b583ba947
Md. Rifat Hasan Jihan <31377578+RHJihan@users.noreply.github.com>
2025-03-06 23:10:44 +0600

    Update existing incomplete bn-IN layout (#1374)

--- app/src/main/java/helium314/keyboard/latin/App.kt
+++ app/src/main/java/helium314/keyboard/latin/App.kt
@@ -530,6 +530,18 @@ fun checkVersionUpgrade(context: Context) {
             prefs.edit().remove("auto_correction_confidence").putFloat(Settings.PREF_AUTO_CORRECT_THRESHOLD, value).apply()
         }
     }
+   if (oldVersion <= 2310) {
+    listOf(
+        Settings.PREF_ENABLED_SUBTYPES,
+        Settings.PREF_SELECTED_SUBTYPE,
+        Settings.PREF_ADDITIONAL_SUBTYPES
+    ).forEach { key ->
+        val value = prefs.getString(key, "")!!
+        if ("bengali," in value) {
+            prefs.edit().putString(key, value.replace("bengali,", "bengali_inscript,")).apply()
+        }
+    }
+}
     upgradeToolbarPrefs(prefs)
     LayoutUtilsCustom.onLayoutFileChanged() // just to be sure
     prefs.edit { putInt(Settings.PREF_VERSION_CODE, BuildConfig.VERSION_CODE) }
