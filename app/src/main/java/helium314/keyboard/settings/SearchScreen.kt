c77db309ef0ecb47d542a8f211df4c2e01a2975e
eranl <1707552+eranl@users.noreply.github.com>
2025-04-15 03:00:28 +0300

    Fix last item on more settings screens.

--- app/src/main/java/helium314/keyboard/settings/SearchScreen.kt
+++ app/src/main/java/helium314/keyboard/settings/SearchScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -201,9 +202,11 @@ fun <T: Any?> SearchScreen(
                 }
             } else {
                 val items = filteredItems(searchText.text)
-                LazyColumn {
-                    items(items) {
-                        itemContent(it)
+                Scaffold { innerPadding ->
+                    LazyColumn(contentPadding = PaddingValues.Absolute(bottom = innerPadding.calculateBottomPadding())) {
+                        items(items) {
+                            itemContent(it)
+                        }
                     }
                 }
             }
