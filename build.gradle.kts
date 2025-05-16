35df3e7bae3d8bd87abe1552621f1cc438d46403
Helium314 <helium314@mailbox.org>
2025-05-16 21:09:16 +0200

    upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion = "2.1.10"
+    val kotlinVersion = "2.1.21"
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.9.2")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
 
         // NOTE: Do not place your application dependencies here; they belong
