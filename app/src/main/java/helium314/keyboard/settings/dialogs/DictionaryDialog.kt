05ea8b7f76af00e9e41ba0eb19581e4f1a21c401
Helium314 <helium314@mailbox.org>
2025-05-24 15:43:44 +0200

    remove unused strings (and rename a string)

--- app/src/main/java/helium314/keyboard/settings/dialogs/DictionaryDialog.kt
+++ app/src/main/java/helium314/keyboard/settings/dialogs/DictionaryDialog.kt
@@ -3,11 +3,8 @@ package helium314.keyboard.settings.dialogs
 
 import android.content.Intent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,8 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,9 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -79,7 +72,7 @@ fun DictionaryDialog(
                 if (mainDict != null)
                     DictionaryDetails(mainDict)
                 if (addonDicts.isNotEmpty()) {
-                    PreferenceCategory(stringResource(R.string.dictionary_settings_title))
+                    PreferenceCategory(stringResource(R.string.dictionary_category_title))
                     addonDicts.forEach { DictionaryDetails(it) }
                 }
                 val dictString = createDictionaryTextAnnotated(locale)
