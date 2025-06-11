f2ec441f45275a0351a86f16f563960d4a5e2fd8
Helium314 <helium314@mailbox.org>
2025-06-11 21:57:06 +0200

    update version and translations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "helium314.keyboard"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3200
-        versionName = "3.2-beta1"
+        versionCode = 3201
+        versionName = "3.2"
         ndk {
             abiFilters.clear()
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
