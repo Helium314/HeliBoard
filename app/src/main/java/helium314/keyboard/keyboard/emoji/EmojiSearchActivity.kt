104a33ef5091d9244d6e0b2dcfd03c71e45b911e
eranl <1707552+eranl@users.noreply.github.com>
2025-08-16 04:03:08 +0300

    Minor fix

--- app/src/main/java/helium314/keyboard/keyboard/emoji/EmojiSearchActivity.kt
+++ app/src/main/java/helium314/keyboard/keyboard/emoji/EmojiSearchActivity.kt
@@ -138,7 +138,7 @@ class EmojiSearchActivity : ComponentActivity() {
                     val localDensity = LocalDensity.current
                     var heightPx by remember { mutableIntStateOf(0) }
                     Column(modifier = Modifier.wrapContentHeight().background(Color(colors.get(ColorType.MAIN_BACKGROUND)))
-                        .clickable(onClick = { }).onGloballyPositioned {
+                        .clickable(false) {}.onGloballyPositioned {
                             val bottom = it.localToScreen(Offset(0f, it.size.height.toFloat())).y.toInt()
                             imeVisible = bottom < screenHeight - 100
                             Log.d("emoji-search", "imeVisible: $imeVisible, imeOpened: $imeOpened, bottom: $bottom, " +
