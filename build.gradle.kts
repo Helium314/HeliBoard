4468faa69921f60906792efd2faece8821d966cf
bcornec44 <58662451+bcornec44@users.noreply.github.com>
2025-09-21 15:36:59 +0200

    upgrade gladle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.13.0")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
 
         // NOTE: Do not place your application dependencies here; they belong
