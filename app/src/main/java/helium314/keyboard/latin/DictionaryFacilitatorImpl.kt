2277fd0cc76e82df1f666df996a3cdf1844b3b10
Eran Leshem <1707552+eranl@users.noreply.github.com>
2025-10-13 22:01:38 +0300

    Fix for `NoSuchElementException` in `DictionaryFacilitatorImpl` (#1997)
    was introduced in #1919

--- app/src/main/java/helium314/keyboard/latin/DictionaryFacilitatorImpl.kt
+++ app/src/main/java/helium314/keyboard/latin/DictionaryFacilitatorImpl.kt
@@ -497,20 +497,23 @@ class DictionaryFacilitatorImpl : DictionaryFacilitator {
             suggestionResults.mRawSuggestions?.addAll(it)
         }
 
-        // Ensure at least two non-emoji, non-typed word results, so that the first two shown suggestions can be non-emoji
+        // Include at least two non-emoji, non-typed word results if possible, so that the first two shown suggestions can be non-emoji
         if (suggestionResults.size > 2) {
-            val nonEmojiCount = suggestionResults.count { isNonEmojiNonTypedWord(it, composedData) }
-            if (nonEmojiCount < 2) {
+            val nonEmojiNonTypedWordCount = suggestionResults.count { !isEmojiOrTypedWord(it, composedData) }
+            if (nonEmojiNonTypedWordCount < 2) {
                 val allResults = SuggestionResults(Int.MAX_VALUE, ngramContext.isBeginningOfSentenceContext, false)
                 suggestionsArray.forEach {
                     if (it == null) return@forEach
                     allResults.addAll(it)
                 }
-                for (i in 0 until 2 - nonEmojiCount) {
-                    val lastEmoji = suggestionResults.last { it.isEmoji }
-                    suggestionResults.remove(lastEmoji)
-                    val firstNonEmoji = allResults.first { !suggestionResults.contains(it) && isNonEmojiNonTypedWord(it, composedData) }
-                    suggestionResults.add(firstNonEmoji)
+                for (i in 0 until 2 - nonEmojiNonTypedWordCount) {
+                    allResults.firstOrNull { !suggestionResults.contains(it) && !isEmojiOrTypedWord(it, composedData) }
+                        ?.let { firstNonEmojiNonTypedWord ->
+                            // The conditions above guarantee that there are at least two EmojiOrTypedWord items
+                            val lastEmojiOrTypedWord = suggestionResults.last { isEmojiOrTypedWord(it, composedData) }
+                            suggestionResults.remove(lastEmojiOrTypedWord)
+                            suggestionResults.add(firstNonEmojiNonTypedWord)
+                        }
                 }
             }
         }
@@ -518,8 +521,8 @@ class DictionaryFacilitatorImpl : DictionaryFacilitator {
         return suggestionResults
     }
 
-    private fun isNonEmojiNonTypedWord(info: SuggestedWordInfo, composedData: ComposedData): Boolean =
-        !info.isEmoji && info.word.compareTo(composedData.mTypedWord, true) != 0
+    private fun isEmojiOrTypedWord(info: SuggestedWordInfo, composedData: ComposedData): Boolean =
+        info.isEmoji || info.word.compareTo(composedData.mTypedWord, true) == 0
 
     private fun getSuggestions(
         composedData: ComposedData, ngramContext: NgramContext,
