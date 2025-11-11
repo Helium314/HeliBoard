fa1f07e5ceefb5c6e2880f0fd0fa5adb106cc6a5
Helium314 <helium314@mailbox.org>
2025-11-11 17:02:55 +0100

    update creation of annotatedstring

--- app/src/main/java/helium314/keyboard/settings/preferences/SwitchPreference.kt
+++ app/src/main/java/helium314/keyboard/settings/preferences/SwitchPreference.kt
@@ -11,14 +11,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.buildAnnotatedString
 import helium314.keyboard.latin.R
 import helium314.keyboard.latin.common.Links
 import helium314.keyboard.latin.utils.DictionaryInfoUtils
 import helium314.keyboard.latin.utils.Log
-import helium314.keyboard.latin.utils.appendLink
 import helium314.keyboard.latin.utils.getActivity
+import helium314.keyboard.latin.utils.htmlToAnnotated
 import helium314.keyboard.latin.utils.prefs
+import helium314.keyboard.latin.utils.withHtmlLink
 import helium314.keyboard.settings.Setting
 import helium314.keyboard.settings.SettingsActivity
 import helium314.keyboard.settings.dialogs.InfoDialog
@@ -85,13 +85,9 @@ fun SwitchPreferenceWithEmojiDictWarning(setting: Setting, default: Boolean) {
     SwitchPreference(setting, default) { showWarningDialog = it && DictionaryInfoUtils.getLocalesWithEmojiDicts(context).isEmpty() }
     if (showWarningDialog) {
         // emoji_dictionary_required contains "%s" since we didn't supply a formatArg
-        val message = stringResource(R.string.emoji_dictionary_required)
-        val annotated = buildAnnotatedString {
-            append(message.substringBefore("%s"))
-            appendLink(stringResource(R.string.dictionary_link_text), Links.DICTIONARY_URL + Links.DICTIONARY_DOWNLOAD_SUFFIX
-                + Links.DICTIONARY_EMOJI_CLDR_SUFFIX)
-            append(message.substringAfter("%s"))
-        }
-        InfoDialog(annotated, onDismissRequest = { showWarningDialog = false })
+        val link = stringResource(R.string.dictionary_link_text).withHtmlLink(Links.DICTIONARY_URL + Links.DICTIONARY_DOWNLOAD_SUFFIX.replace("raw", "src")
+            + Links.DICTIONARY_EMOJI_CLDR_SUFFIX)
+        val message = stringResource(R.string.emoji_dictionary_required, link)
+        InfoDialog(message.htmlToAnnotated(), onDismissRequest = { showWarningDialog = false })
     }
 }
