d7ec783c95f1ab77f39a99ada0853f5a35e2d52e
Helium314 <helium314@mailbox.org>
2025-11-24 16:24:51 +0100

    be a bit more restrictive with firefox workarounds
    fixes GH-2133

--- app/src/main/java/helium314/keyboard/compat/AppWorkarounds.kt
+++ app/src/main/java/helium314/keyboard/compat/AppWorkarounds.kt
@@ -7,15 +7,22 @@ import helium314.keyboard.latin.utils.InputTypeUtils
 
 object AppWorkarounds {
     fun adjustInputType(inputType: Int, packageName: String?): Int {
-        if (inputType and InputType.TYPE_MASK_CLASS != InputType.TYPE_CLASS_TEXT) return inputType
-        if (InputTypeUtils.isAnyPasswordInputType(inputType)) return inputType
         return when (packageName) {
-            // Firefox and forks (assuming all of them) don't set these flags, so we simply force them for all text fields in the app
-            // missing TYPE_TEXT_VARIATION_WEB_EDIT_TEXT is strange, considering (almost) all fields except URL bar should set it
-            // missing TYPE_TEXT_FLAG_NO_SUGGESTIONS is horrible, because JavaScript stuff does not interact properly with composing region
             "org.mozilla.fennec_fdroid", "org.mozilla.fenix", "org.mozilla.firefox_beta", "org.mozilla.focus",
             "org.mozilla.klar", "org.mozilla.firefox", "org.ironfoxoss.ironfox", "net.waterfox.android.release",
-            "io.github.forkmaintainers.iceraven", "com.zen.web.tools.browser" -> inputType or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT
+            "io.github.forkmaintainers.iceraven", "com.zen.web.tools.browser" -> {
+                // Firefox and forks (assuming all of them) don't set these flags, so we want to force them for most text fields on websites
+                // missing TYPE_TEXT_VARIATION_WEB_EDIT_TEXT is strange, considering all text fields on web pages should set it
+                // missing TYPE_TEXT_FLAG_NO_SUGGESTIONS is horrible, because JS does not interact properly with composing region
+                if (inputType and InputType.TYPE_MASK_CLASS != InputType.TYPE_CLASS_TEXT) return inputType
+                if (inputType and InputType.TYPE_MASK_VARIATION != 0) return inputType // if any variation is specified we leave it (URL, email, password, ...)
+                // looks like most (all?) non-password text fields on websites are either IME_MULTI_LINE or IME_MULTI_LINE + AUTO_CORRECT + CAP_SENTENCES
+                if (inputType and InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE == 0) return inputType
+                // for the AUTO_CORRECT flag we assume suggestions are safe and only add WEB_EDIT_TEXT
+                if (inputType and InputType.TYPE_TEXT_FLAG_AUTO_CORRECT == 0) return inputType or InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT
+                // for all others we also add NO_SUGGESTIONS to avoid JS messing with the composing text
+                inputType or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT
+            }
             else -> inputType
         }
     }
